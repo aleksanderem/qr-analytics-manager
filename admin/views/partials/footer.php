d85bf2dbe41102ae546191c28ae608121132fcfd
aleksanderem <amiesak@gmail.com>
2025-12-17 13:25:01 +0100

    Update BMC button - Poppins font, full text

--- admin/views/partials/footer.php
+++ admin/views/partials/footer.php
@@ -8,5 +8,5 @@ if (!defined('ABSPATH')) {
 }
 ?>
 <div class="qr-analytics-footer" style="margin-top: 30px; padding: 15px 0; border-top: 1px solid #ddd; text-align: left;">
-    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="alexem" data-color="#FFDD00" data-emoji="☕" data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" data-height="32"></script>
+    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="alexem" data-color="#FFDD00" data-emoji="☕" data-font="Poppins" data-text="You like it? Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff"></script>
 </div>
