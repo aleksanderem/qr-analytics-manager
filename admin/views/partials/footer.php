05817efa5a8eae09b83129e65909d9abfc279886
aleksanderem <amiesak@gmail.com>
2025-12-17 13:24:20 +0100

    Make BMC button smaller and left-aligned

--- admin/views/partials/footer.php
+++ admin/views/partials/footer.php
@@ -7,6 +7,6 @@ if (!defined('ABSPATH')) {
     exit;
 }
 ?>
-<div class="qr-analytics-footer" style="margin-top: 30px; padding: 20px 0; border-top: 1px solid #ddd; text-align: center;">
-    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="alexem" data-color="#FFDD00" data-emoji="☕" data-font="Cookie" data-text="You like it? Buy me a coffe" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff"></script>
+<div class="qr-analytics-footer" style="margin-top: 30px; padding: 15px 0; border-top: 1px solid #ddd; text-align: left;">
+    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="alexem" data-color="#FFDD00" data-emoji="☕" data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" data-height="32"></script>
 </div>
